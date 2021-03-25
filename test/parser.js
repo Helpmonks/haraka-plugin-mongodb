5c7cdcb791383716c6105ce0b598bf3a790081f7
Nitai <nitai@helpmonks.com>
2021-03-24 20:15:54 -0400

    Added option to use redis for incoming limits

--- test/parser.js
+++ test/parser.js
@@ -2,8 +2,8 @@ const simpleParser = require('mailparser').simpleParser;
 const fs = require('fs-extra');
 
 
-const source_haraka = fs.readFileSync('./test_haraka_body.json');
-const source_file = fs.readFileSync('./test.eml');
+const source_haraka = fs.readFileSync('./test_data/test_haraka_body.json');
+const source_file = fs.readFileSync('./test_data/test.eml');
 
 simpleParser(source_file, {}, (err, parsed) => {
 	console.log("err", err);
